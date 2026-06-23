8894f52afb271c9adfeb5e9148aa42b2f4374033
Yuva Deepika Bolleddu <yuvadeepikadeepu@gmail.com>
2026-06-23 18:47:37 +0000

    commit

---
+++ models/example/ecomm/staging/stg_customer_addresses.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='view') }} 
+
+
+select
+    address_id,
+    customer_id,
+    ddress_type,
+    street,
+    city,
+    state,
+    pincode,
+    is_default,
+    case
+        when address_type = 'shipping' then true
+        else false 
+    end as is_shipping
+from {{source('customers','customer_addresses')}}
---
+++ models/example/ecomm/staging/stg_order_status_history.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='view') }} 
+
+
+select
+    history_id,
+    order_id,
+    status,
+    changed_at,
+    changed_by
+    from {{source('orders','order_status_history')}}
