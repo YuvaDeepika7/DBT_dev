3dfc0834359fcf45a8bcdad976a24b0d04457138
Yuva Deepika Bolleddu <yuvadeepikadeepu@gmail.com>
2026-06-26 11:02:59 +0000

    commit

--- models/example/ecomm/incremental/fct_daily_sales.sql
+++ models/example/ecomm/incremental/fct_daily_sales.sql
@@ -7,19 +7,20 @@
 }} 
 
 
-select {{ dbt_utils.generate_surrogate_key(['order_date']) }} 
+select 
+     order_date, 
+    count(order_id)                     as total_orders,
+    sum(discount_amount)                as total_discounts, 
+    avg(net_amount)                     as avg_order_value, 
+    current_timestamp                   as loaded_at, 
+     {{ dbt_utils.generate_surrogate_key(['order_date']) }} 
                                         as daily_sales_id, 
-    order_date, 
-    count(order_id)                     as total_orders, 
     count(case when status = 'completed' 
         then 1 end)                     as completed_orders, 
     count(case when status = 'returned' 
         then 1 end)                     as returned_orders, 
     sum(case when status = 'completed' 
-        then net_amount else 0 end)      as gross_revenue, 
-    sum(discount_amount)                as total_discounts, 
-    avg(net_amount)                     as avg_order_value, 
-    current_timestamp                   as loaded_at 
+        then net_amount else 0 end)      as gross_revenue 
 from {{ ref('stg_orders_ecomm') }} 
 
 
